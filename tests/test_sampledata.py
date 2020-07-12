1adeb7738709b0c635dbee7415d4e97ce6a61669
Joel Horowitz <joelhoro@gmail.com>
2020-07-11 21:04:17 -0400

    Fixing test

--- tests/test_sampledata.py
+++ tests/test_sampledata.py
@@ -4,9 +4,8 @@ from sample_data import countries
 class TestStringMethods(unittest.TestCase):
 
     def test_countries(self):
-        #self.assertEqual('foo'.upper(), 'FOO')
         df = countries()
-        self.assertEqual(len(df),200,"Countries dataframe has the wrong size")
+        self.assertEqual(len(df),279,"Countries dataframe has the wrong size")
 
 if __name__ == '__main__':
     unittest.main()
