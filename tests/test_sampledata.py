524bf77f3a592ddc0525ddde4759bce45e604ded
Joel Horowitz <joelhoro@gmail.com>
2020-07-12 10:09:38 -0400

    More verbose tests

--- tests/test_sampledata.py
+++ tests/test_sampledata.py
@@ -4,11 +4,16 @@ from sample_data import countries, airbnb
 class TestFiles(unittest.TestCase):
 
     def test_countries(self):
+        print("Testing countries")
         df = countries()
+        print(df.head())
         self.assertEqual(len(df),279,"Countries dataframe has the wrong size")
 
-    def test_drinks(self):
+    def test_airbnb(self):
+        print("Testing airbnb")
         df = airbnb()
+        print(df.head())
+        
         for key in ['Manhattan','Queens','Brooklyn']:
             rows = df[df['Neighbourhood '] == key]
             self.assertGreater(len(rows),0, f"No data for {key}")    
