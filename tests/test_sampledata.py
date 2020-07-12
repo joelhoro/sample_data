4ad59042d2d416b2773fc2894ae61d5c2bd0b158
Joel Horowitz <joelhoro@gmail.com>
2020-07-12 10:01:40 -0400

    Added tests

--- tests/test_sampledata.py
+++ tests/test_sampledata.py
@@ -1,11 +1,17 @@
 import unittest
-from sample_data import countries
+from sample_data import countries, airbnb
 
-class TestStringMethods(unittest.TestCase):
+class TestFiles(unittest.TestCase):
 
     def test_countries(self):
         df = countries()
         self.assertEqual(len(df),279,"Countries dataframe has the wrong size")
+
+    def test_drinks(self):
+        df = airbnb()
+        for key in ['Manhattan','Queens','Brooklyn']:
+            rows = df[df['Neighbourhood '] == key]
+            self.assertGreater(len(rows),0, f"No data for {key}")    
 
 if __name__ == '__main__':
     unittest.main()
